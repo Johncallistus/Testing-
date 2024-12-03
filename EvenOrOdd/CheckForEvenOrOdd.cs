208b68ac78278811761848e28ba9b374bdbfd24b
Johncallistus <jcallistus@gmail.com>
2024-12-03 18:02:09 -0500

    new

---
+++ EvenOrOdd/CheckForEvenOrOdd.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenOrOdd
+{
+    public static class CheckForEvenOrOdd
+    {
+     
+        public static string EvenOdd(int i)
+        {
+            if (i % 2 == 0)
+            {
+                return ("The entered number is an Even Number");
+                
+            }
+            else
+            {
+                return ("The entered number is an Odd Number");
+                
+            }
+           }
+
+    }
+}
---
+++ EvenOrOdd/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace EvenOrOddConsole
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int i;
+            Console.Write("Please enter an Integer: ");
+            i = int.Parse(Console.ReadLine());
+            string output = EvenOrOdd.CheckForEvenOrOdd.EvenOdd(i);
+            Console.WriteLine(output);
+            Console.ReadLine();
+
+        }
+    }
+}
